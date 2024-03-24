0bf14406dfdebc77e4d9b914f2e233ac5bf8a365
peterwalcker <87015108+peterwalcker@users.noreply.github.com>
2024-03-24 18:45:51 +0300

    1

---
+++ VoiceTextBot/Core/Classes/Bot.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Microsoft.Extensions.Hosting;
+
+namespace VoiceTextBot.Core.Classes
+{
+    class Bot : BackgroundService
+    {
+        private ITelegramBotClient _telegramBotClient;
+
+        public Bot(ITelegramBotClient telegramBotClient)
+        {
+            _telegramBotClient = telegramBotClient;
+        }
+
+        async Task HandlerUpdateAsync(ITelegramBotClient client, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type == UpdateType.CallbackQuery)
+            {
+                await _telegramBotClient.SendTextMessageAsync(update.Message.Chat.Id,
+                                                                "you pushed the button",
+                                                                cancellationToken: cancellationToken);
+                return;
+            }
+
+            if (update.Type == UpdateType.Message)
+            {
+                await _telegramBotClient.SendTextMessageAsync(update.Message.Chat.Id,
+                                                                "you sent text message",
+                                                                cancellationToken: cancellationToken);
+                return;
+            }
+        }
+
+        Task HandleErrorAsync(ITelegramBotClient client, Exception exception, CancellationToken cancellationToken)
+        {
+            var errorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine($"Error: {errorMessage}");
+
+            Console.WriteLine("Waiting for 10 seconds for reconnect.");
+            Thread.Sleep(10000);
+
+            return Task.CompletedTask;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _telegramBotClient.StartReceiving(
+                HandlerUpdateAsync,
+                HandleErrorAsync,
+                new Telegram.Bot.Polling.ReceiverOptions() { AllowedUpdates = { } },
+                cancellationToken: stoppingToken);
+
+            Console.WriteLine("Bot started!");
+        }
+    }
+}
---
+++ VoiceTextBot/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using VoiceTextBot.Core.Classes;
+
+namespace VoiceTextBot
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services, args[0]))
+                .UseConsoleLifetime()
+                .Build();
+
+            Console.WriteLine("Service started!");
+            await host.RunAsync();
+            Console.WriteLine("Service stopped!");
+        }
+
+        static void ConfigureServices(IServiceCollection services, string token)
+        {
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient(token));
+            services.AddHostedService<Bot>();
+        }
+    }
+}
